5aed740b95a371ad551d848018610001b3919853
vwkd <33468089+vwkd@users.noreply.github.com>
2024-02-20 20:36:16 +0400

    data: adjust inflation manually

--- src/data/grundfreibetrag.ts
+++ src/data/grundfreibetrag.ts
@@ -1,5 +1,4 @@
 // Quelle: https://de.wikipedia.org/wiki/Grundfreibetrag_(Deutschland)
-// ?? Realwert in 2024
 export const values = [
   {
     "Jahr": 2002,
@@ -7,29 +6,14 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2002,
-    "Wert": 10_690,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2003,
     "Wert": 7_235,
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2003,
-    "Wert": 10_690,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2004,
     "Wert": 7_664,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2004,
-    "Wert": 10_290,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2005,
@@ -37,19 +21,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2005,
-    "Wert": 10_290,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2006,
     "Wert": 7_664,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2006,
-    "Wert": 10_290,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2007,
@@ -57,19 +31,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2007,
-    "Wert": 10_290,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2008,
     "Wert": 7_664,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2008,
-    "Wert": 10_290,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2009,
@@ -77,19 +41,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2009,
-    "Wert": 10_490,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2010,
     "Wert": 8_004,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2010,
-    "Wert": 10_180,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2011,
@@ -97,19 +51,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2011,
-    "Wert": 10_180,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2012,
     "Wert": 8_004,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2012,
-    "Wert": 10_180,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2013,
@@ -117,19 +61,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2013,
-    "Wert": 10_190,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2014,
     "Wert": 8_354,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2014,
-    "Wert": 10_370,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2015,
@@ -137,19 +71,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2015,
-    "Wert": 10_460,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2016,
     "Wert": 8_652,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2016,
-    "Wert": 10_630,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2017,
@@ -157,19 +81,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2017,
-    "Wert": 10_680,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2018,
     "Wert": 9_000,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2018,
-    "Wert": 10_700,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2019,
@@ -177,19 +91,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2019,
-    "Wert": 10_750,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2020,
     "Wert": 9_408,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2020,
-    "Wert": 10_980,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2021,
@@ -197,19 +101,9 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2021,
-    "Wert": 11_030,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2022,
     "Wert": 10_347,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2022,
-    "Wert": 10_960,
-    "Wertart": "Realwert",
   },
   {
     "Jahr": 2023,
@@ -217,17 +111,8 @@ export const values = [
     "Wertart": "Nominalwert",
   },
   {
-    "Jahr": 2023,
-    "Wert": 10_910,
-    "Wertart": "Realwert",
-  },
-  {
     "Jahr": 2024,
     "Wert": 11_784,
     "Wertart": "Nominalwert",
-  },
-  {
-    "Jahr": 2024,
-    "Wertart": "Realwert",
   },
 ];
