313cbacd730b35778075934080a6289d81057313
vwkd <33468089+vwkd@users.noreply.github.com>
2024-03-07 23:33:48 +0400

    data: add all Grundfreibetrag

--- src/data/grundfreibetrag.ts
+++ src/data/grundfreibetrag.ts
@@ -5,6 +5,226 @@
  */
 export const values = [
   {
+    "Jahr": 1958,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1959,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1960,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1961,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1962,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1963,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1964,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1965,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1966,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1967,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1968,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1969,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1970,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1971,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1972,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1973,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1974,
+    "Wert": 1_680,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1975,
+    "Wert": 3_029,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1976,
+    "Wert": 3_029,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1977,
+    "Wert": 3_029,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1978,
+    "Wert": 3_329,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1979,
+    "Wert": 3_690,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1980,
+    "Wert": 3_690,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1981,
+    "Wert": 4_212,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1982,
+    "Wert": 4_212,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1983,
+    "Wert": 4_212,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1984,
+    "Wert": 4_212,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1985,
+    "Wert": 4_212,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1986,
+    "Wert": 4_536,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1987,
+    "Wert": 4_536,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1988,
+    "Wert": 4_752,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1989,
+    "Wert": 4_752,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1990,
+    "Wert": 5_616,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1991,
+    "Wert": 5_616,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1992,
+    "Wert": 5_616,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1993,
+    "Wert": 5_616,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1994,
+    "Wert": 5_616,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1995,
+    "Wert": 5_616,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1996,
+    "Wert": 12_095,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1997,
+    "Wert": 12_095,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1998,
+    "Wert": 12_365,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 1999,
+    "Wert": 13_067,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 2000,
+    "Wert": 13_499,
+    "Wertart": "Nominalwert",
+  },
+  {
+    "Jahr": 2001,
+    "Wert": 14_039,
+    "Wertart": "Nominalwert",
+  },
+  {
     "Jahr": 2002,
     "Wert": 7_235,
     "Wertart": "Nominalwert",
